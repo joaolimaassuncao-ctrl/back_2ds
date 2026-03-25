abf0fa67560b9ce9183e800f4edb587acdd80045
joaolimaassuncao-ctrl <joao.lima.assuncao@escola.pr.gov.br>
2026-03-25 11:36:55 +0000

    finalizar de objetos.js

---
+++ objetos/obj-01.js
@@ -0,0 +1,17 @@
+//Objetos são estruturas chave-valor
+//tambem chamadas "key-value"
+//objetos podem conter: arrays,string,
+//number, objects, function...
+//possuem metodos proprios
+
+const jogador = { 
+  nome: "João Victor",
+  gols: 1900,
+  time: ['Coritiba', 'Manchester United', 'Real Madrid'],
+  Prime: 2026
+
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.time[2])
+console.log(jogador.Prime)
---
+++ objetos/obj-02.js
@@ -0,0 +1,15 @@
+const carro = { 
+  marca: "Chevrolet",
+  ano: 1967,
+  modelo: "impala",
+  motor: "V8",
+  cor: "Preto",
+  cavalos: 425 
+
+}
+console.log(carro.marca)
+console.log(carro.ano)
+console.log(carro.modelo)
+console.log(carro.motor)
+console.log(carro.cor)
+console.log(carro.cavalos)
