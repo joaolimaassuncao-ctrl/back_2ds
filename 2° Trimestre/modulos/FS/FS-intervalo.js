376639ba4461709842daf1d87f100a678958d9b7
joaolimaassuncao-ctrl <joao.lima.assuncao@escola.pr.gov.br>
2026-08-12 11:49:54 +0000

    refactor: reorganize file structure and implement server functionality

---
+++ 2° Trimestre/modulos/FS/FS-intervalo.js
@@ -0,0 +1,10 @@
+// setTimeout (ms) - executa uma vez
+setTimeout(function(){
+console.log('Executou timeout...')
+}, 1000)
+
+// setInterval (ms) - executa 'x' vezes 
+setInterval(function(){
+console.log('Excutou FPS 30')
+}, 1000)
+    
---
+++ 2° Trimestre/modulos/mod01/mod03-server/server1.js
@@ -0,0 +1,38 @@
+const http = require('node:http')
+const path = require('node:path')
+const fs = require('node:fs')
+const porta = 8005
+const home = path.join(__dirname, 'pages/index.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
+const erro = path.join(__dirname, 'pages/404.html')
+//qual informação eu tenho?
+
+const server = http.createServer((Req, res) => {
+  const urlTratada = new URL(Req.url, `http://${Req.headers.host}`)
+    const recurso = urlTratada.pathname
+
+ if (recurso === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/html; charset=utf-8')
+    return res.end(fs.readFileSync(home, 'utf-8'))
+
+ } if (recurso === '/sobre') {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/html; charset=utf-8')
+    return res.end(fs.readFileSync(sobre, 'utf-8'))
+  } 
+  if (recurso === '/404') {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/html; charset=utf-8')
+    return res.end(fs.readFileSync(404, 'utf-8'))
+  }else {
+    res.setHeader('Content-typer', 'text/html; charset=utf-8')
+    res.statusCode = 401
+    res.end('<h3>401 Não Autorizado!</h3>')
+  }
+
+})
+
+server.listen(porta, () => {
+  console.log(`servidor rodando na porta ${porta}`)
+})
